0650fdedb6bc7637ee2e621c1236666960bc5930
Seif Mohamed <seif.eissa7797@gmail.com>
2026-03-03 19:15:26 +0200

    feat: Add SqlConnectionFactory and new type handler to handle DateOnly in Dapper

---
+++ src/Bookify.Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,37 @@
+﻿using Bookify.Application.Abstractions.Data;
+using Npgsql;
+using System.Data;
+
+namespace Bookify.Infrastructure.Data;
+/// <summary>
+/// Factory responsible for creating raw SQL database connections.
+/// Used mainly for Dapper or read-only queries (CQRS - Query side).
+/// </summary>
+internal sealed class SqlConnectionFactory : ISqlConnectionFactory
+{
+    private readonly string _connectionString;
+
+    /// <summary>
+    /// Initializes the factory with the database connection string.
+    /// </summary>
+    public SqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    /// <summary>
+    /// Creates and opens a new database connection.
+    /// Caller is responsible for disposing the connection.
+    /// </summary>
+    public IDbConnection CreateConnection()
+    {
+        // Create PostgreSQL connection
+        var connection = new NpgsqlConnection(_connectionString);
+
+        // Open connection immediately
+        // (Important for Dapper usage)
+        connection.Open();
+
+        return connection;
+    }
+}
