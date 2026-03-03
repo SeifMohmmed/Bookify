532ad5edae173aefd3ed322f362a9e53e572aca2
Seif Mohamed <seif.eissa7797@gmail.com>
2026-03-03 18:34:58 +0200

    Adding Domain Entities Configurations

---
+++ src/Bookify.Infrastructure/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Reviews;
+using Bookify.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations;
+internal sealed class ReviewConfiguration : IEntityTypeConfiguration<Review>
+{
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder.ToTable("reviews");
+
+        builder.HasKey(x => x.Id);
+
+        builder.OwnsOne(x => x.Rating);
+
+        builder.Property(x => x.Comment)
+            .HasMaxLength(200);
+
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(x => x.ApartmentId);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(x => x.UserId);
+
+        builder.HasOne<Booking>()
+            .WithMany()
+            .HasForeignKey(x => x.BookingId);
+    }
+}
