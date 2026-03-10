368d7410109f8799492fa3b19c6c556a3fc6387a
Seif Mohamed <seif.eissa7797@gmail.com>
2026-03-10 02:55:33 +0200

    Add ntegration Testing

---
+++ test/Bookify.Application.IntegrationTests/Bookings/ConfirmBookingTests.cs
@@ -0,0 +1,28 @@
+﻿using Bookify.Application.Bookings.GetBooking;
+using Bookify.Application.IntegrationTests.Infrastructure;
+using Bookify.Domain.Bookings;
+using FluentAssertions;
+
+namespace Bookify.Application.IntegrationTests.Bookings;
+public class ConfirmBookingTests : BaseIntegrationTest
+{
+    private static readonly Guid BookingId = Guid.NewGuid();
+
+    public ConfirmBookingTests(IntegrationTestWebAppFactory webAppFactory)
+        : base(webAppFactory)
+    {
+    }
+
+    [Fact]
+    public async Task ConfirmBooking_ShouldReturnFailure_WhenBookingIsNotFound()
+    {
+        // Arrange
+        var command = new GetBookingQuery(BookingId);
+
+        // Act
+        var result = await Sender.Send(command);
+
+        // Assert
+        result.Error.Should().Be(BookingErrors.NotFound);
+    }
+}
